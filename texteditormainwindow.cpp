2e534fbc421a700b84df80486f48e5befe83b5aa
Thiago <bliscosque@yahoo.com.br>
2022-08-11 21:26:04 -0500

    adding other edit functions

--- texteditormainwindow.cpp
+++ texteditormainwindow.cpp
@@ -17,6 +17,7 @@ TextEditorMainWindow::~TextEditorMainWindow()
 
 void TextEditorMainWindow::on_actionNew_triggered()
 {
+    localfilename="";
     ui->textEdit->clear();
     ui->textEdit->setFocus();
 }
@@ -38,5 +39,90 @@ void TextEditorMainWindow::on_actionOpen_triggered()
         ui->textEdit->setText(text);
     }
     file.close();
+}
+
+
+void TextEditorMainWindow::on_actionSave_as_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "Save as...", QDir::homePath(), file_types);
+    localfilename = filename;
+
+    if(localfilename == "") {
+        return;
+    }
+    QFile file(filename);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        if (file.exists()) {
+            QMessageBox::warning(this, "Error saving file", "File cannot be saved");
+        }
+        return;
+    }
+    QTextStream out_file(&file);
+    QString text=ui->textEdit->toPlainText();
+    out_file << text;
+    file.flush();
+    file.close();
+}
+
+
+void TextEditorMainWindow::on_actionSave_triggered()
+{
+    QString filename;
+    if (localfilename=="") {
+        filename = QFileDialog::getSaveFileName(this, "Save as...", QDir::homePath(), file_types);
+        localfilename = filename;
+    } else {
+        filename=localfilename;
+    }
+    QFile file(filename);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        if (file.exists()) {
+            QMessageBox::warning(this, "Error saving file", "File cannot be saved");
+        }
+        return;
+    }
+    QTextStream out_file(&file);
+    QString text=ui->textEdit->toPlainText();
+    out_file << text;
+    file.flush();
+    file.close();
+}
+
+
+void TextEditorMainWindow::on_actionQuit_triggered()
+{
+    close();
+}
+
+
+void TextEditorMainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+
+void TextEditorMainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void TextEditorMainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void TextEditorMainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void TextEditorMainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
 }
 
--- texteditormainwindow.h
+++ texteditormainwindow.h
@@ -18,13 +18,29 @@ class TextEditorMainWindow : public QMainWindow
 public:
     TextEditorMainWindow(QWidget *parent = nullptr);
     ~TextEditorMainWindow();
-    QString localfilename;
+    QString localfilename="";
     QString file_types = "Text files (*.txt) ;; Markdown files (*.md)";
 
 private slots:
     void on_actionNew_triggered();
 
     void on_actionOpen_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionQuit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
 
 private:
     Ui::TextEditorMainWindow *ui;
